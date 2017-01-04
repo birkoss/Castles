ce3c3c74a24e70917c474b03429fb6fe0ba4b235
Mathieu <mathieu@standish.ca>
2017-01-04 13:58:57 -0500

    Size ok

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(320, 480, Phaser.AUTO, '');
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, '');
 
 game.state.add('Menu', Menu);
 game.state.add('Game', Game);
